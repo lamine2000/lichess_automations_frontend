6114ea2da9913b9e7975ab1570a5cff3b1dbfe10
Mouhamadou Lamine Gueye <lemzogueye.lg72@gmail.com>
2021-11-19 23:32:24 +0000

    initializing project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'lichess-automations',
+    appId: '1:712278659371:web:06622d2f5b16167caf756e',
+    storageBucket: 'lichess-automations.appspot.com',
+    apiKey: 'AIzaSyCWc5rakr5lfiRlqK7KUNx5fMCAAhWZdTg',
+    authDomain: 'lichess-automations.firebaseapp.com',
+    messagingSenderId: '712278659371',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'lichess-automations',
+    appId: '1:712278659371:web:06622d2f5b16167caf756e',
+    storageBucket: 'lichess-automations.appspot.com',
+    apiKey: 'AIzaSyCWc5rakr5lfiRlqK7KUNx5fMCAAhWZdTg',
+    authDomain: 'lichess-automations.firebaseapp.com',
+    messagingSenderId: '712278659371',
+  },
   production: false
 };
 
