43fdc0e9c71db3648adf864118dceb959a0554f9
Mouhamadou Lamine Gueye <lemzogueye.lg72@gmail.com>
2021-11-20 21:47:33 +0000

    initializing project

---
+++ src/app/components/modify-token/modify-token.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-token',
+  templateUrl: './modify-token.component.html',
+  styleUrls: ['./modify-token.component.css']
+})
+export class ModifyTokenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/model/user.model.ts
@@ -0,0 +1,5 @@
+export interface User {
+  uid: string,
+  email: string,
+  lichess_token?: string
+}
